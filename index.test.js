42defb75a333028639d732f21905bec39f3ae794
laraibxsunny <laraibsunny373@yahoo.co.uk>
2024-11-04 16:43:10 +0000

    Part 1: Done

--- index.test.js
+++ index.test.js
@@ -1,22 +1,52 @@
 // install dependencies
-const { execSync } = require('child_process');
-execSync('npm install');
-execSync('npm run seed');
+const { execSync } = require("child_process");
+execSync("npm install");
+execSync("npm run seed");
 
-const request = require("supertest")
-const { db } = require('./db/connection');
-const { Musician } = require('./models/index')
-const app = require('./src/app');
-const {seedMusician} = require("./seedData");
+const request = require("supertest");
+const { db } = require("./db/connection");
+const { Musician } = require("./models/index");
+const app = require("./src/app");
+const { seedMusician } = require("./seedData");
 
+describe("./musicians endpoint", () => {
+  test("Testing The Endpoint", async () => {
+    //Arrange / Act
+    const response = await request(app).get("/musicians");
+    //Assert
+    expect(response.statusCode).toBe(200);
+  });
+  test("Testing Data Recovery/Accuracy", async () => {
+    //Arrange
+    const response = await request(app).get("/musicians");
 
-describe('./musicians endpoint', () => {
-    // Write your tests here
-    
-    
+    //Act
+    const responseData = JSON.parse(response.text);
 
+    //Assert
+    expect(responseData[0].name).toBe("Mick Jagger");
+    expect(responseData[1].instrument).toBe("Voice");
+    expect(responseData[2].id).toBe(3);
+  });
+});
 
+describe("./bands endpoint", () => {
+  test("Testing The Endpoint", async () => {
+    //Arrange / Act
+    const response = await request(app).get("/bands");
+    //Assert
+    expect(response.statusCode).toBe(200);
+  });
+  test("Testing Data Recovery/Accuracy", async () => {
+    //Arrange
+    const response = await request(app).get("/bands");
 
+    //Act
+    const responseData = JSON.parse(response.text);
 
-    
-})
+    //Assert
+    expect(responseData[0].name).toBe("The Beatles");
+    expect(responseData[1].genre).toBe("Pop");
+    expect(responseData[2].id).toBe(3);
+  });
+});
--- src/app.js
+++ src/app.js
@@ -1,16 +1,33 @@
 const express = require("express");
 const app = express();
-const { Musician } = require("../models/index")
-const { db } = require("../db/connection")
+const { Musician, Band } = require("../models/index");
+const { db } = require("../db/connection");
 
 const port = 3000;
 
-//TODO: Create a GET /musicians route to return all musicians 
+app.get("/musicians", async (request, response) => {
+  const allMusicians = await Musician.findAll();
+  response.json(allMusicians);
+});
 
+app.get("/musicians/1", async (request, response) => {
+  const firstMusician = await Musician.findByPk(1);
+  response.json(firstMusician);
+});
 
+app.get("/musicians/2", async (request, response) => {
+  const secondMusician = await Musician.findByPk(2);
+  response.json(secondMusician);
+});
 
+app.get("/musicians/3", async (request, response) => {
+  const thirdMusician = await Musician.findByPk(3);
+  response.json(thirdMusician);
+});
 
-
-
+app.get("/bands", async (request, response) => {
+  const allBands = await Band.findAll();
+  response.json(allBands);
+});
 
 module.exports = app;
