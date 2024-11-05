81445b3ef85527d89ccb20e73692d3ca2f763dfa
laraibxsunny <laraibsunny373@yahoo.co.uk>
2024-11-05 15:46:22 +0000

    Part 2&3: Done

--- index.test.js
+++ index.test.js
@@ -9,7 +9,8 @@ const { Musician } = require("./models/index");
 const app = require("./src/app");
 const { seedMusician } = require("./seedData");
 
-describe("./musicians endpoint", () => {
+
+describe("/musicians endpoint", () => {
   test("Testing The Endpoint", async () => {
     //Arrange / Act
     const response = await request(app).get("/musicians");
@@ -30,7 +31,7 @@ describe("./musicians endpoint", () => {
   });
 });
 
-describe("./bands endpoint", () => {
+describe("/bands endpoint", () => {
   test("Testing The Endpoint", async () => {
     //Arrange / Act
     const response = await request(app).get("/bands");
@@ -48,5 +49,61 @@ describe("./bands endpoint", () => {
     expect(responseData[0].name).toBe("The Beatles");
     expect(responseData[1].genre).toBe("Pop");
     expect(responseData[2].id).toBe(3);
+  });
+});
+
+describe("/musicians/:id endpoint", () => {
+  test("Testing The Endpoint", async () => {
+    //Arrange / Act
+    const response = await request(app).get("/musicians/1");
+    //Assert
+    expect(response.statusCode).toBe(200);
+  });
+
+  test("Testing Data Recovery/Accuracy", async () => {
+    //Arrange
+    const response1 = await request(app).get("/musicians/1");
+    const response2 = await request(app).get("/musicians/2");
+    const response3 = await request(app).get("/musicians/3");
+
+    //Act
+    const responseData1 = JSON.parse(response1.text);
+    const responseData2 = JSON.parse(response2.text);
+    const responseData3 = JSON.parse(response3.text);
+
+    //Assert
+    expect(responseData1.name).toBe("Mick Jagger");
+    expect(responseData2.instrument).toBe("Voice");
+    expect(responseData3.instrument).toBe("Guitar");
+  });
+
+  describe("Testing CRUD Operations", () => {
+    test("Create/Post Endpoint", async () => {
+      //Arrange /Act
+      const response = await request(app).post("/musicians");
+      //Assert
+      expect(response.statusCode).toBe(200);
+    });
+
+    test("Read/Get Endpoint", async () => {
+      //Arrange /Act
+      const response = await request(app).get("/musicians/1");
+      //Assert
+      expect(response.statusCode).toBe(200);
+    });
+
+    test("Update/Put Endpoint", async () => {
+      //Arrange /Act
+      const response = await request(app).put("/musicians/1");
+      //Assert
+      expect(response.statusCode).toBe(200);
+    });
+
+    test("Delete Endpoint", async () => {
+      //Arrange /Act
+      const response = await request(app).delete("/musicians/3");
+      //Assert
+      expect(response.statusCode).toBe(200);
+    });
   });
 });
--- src/app.js
+++ src/app.js
@@ -5,29 +5,43 @@ const { db } = require("../db/connection");
 
 const port = 3000;
 
+app.use(express.json());
+
+app.use(express.urlencoded());
+
 app.get("/musicians", async (request, response) => {
   const allMusicians = await Musician.findAll();
   response.json(allMusicians);
 });
 
-app.get("/musicians/1", async (request, response) => {
-  const firstMusician = await Musician.findByPk(1);
-  response.json(firstMusician);
-});
-
-app.get("/musicians/2", async (request, response) => {
-  const secondMusician = await Musician.findByPk(2);
-  response.json(secondMusician);
-});
-
-app.get("/musicians/3", async (request, response) => {
-  const thirdMusician = await Musician.findByPk(3);
-  response.json(thirdMusician);
-});
-
 app.get("/bands", async (request, response) => {
   const allBands = await Band.findAll();
   response.json(allBands);
+});
+
+app.get("/musicians/:id", async (req, res) => {
+  const musicianById = await Musician.findByPk(req.params.id);
+  res.json(musicianById);
+});
+
+app.post("/musicians", async (req, res) => {
+  await Musician.create(req.body);
+  const allMusicians = await Musician.findAll();
+  res.json(allMusicians);
+});
+
+app.put("/musicians/:id", async (req, res) => {
+  let updateMusician = await Musician.findByPk(req.params.id);
+  await updateMusician.update(req.body);
+  const allMusicians = await Musician.findAll();
+  res.json(allMusicians);
+});
+
+app.delete("/musicians/:id", async (req, res) => {
+  const deleteMusician = await Musician.findByPk(req.params.id);
+  await deleteMusician.destroy();
+  const allMusicians = await Musician.findAll();
+  res.json(allMusicians);
 });
 
 module.exports = app;
